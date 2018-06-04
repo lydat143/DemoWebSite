2bc9842500f9b41cd29e7565a9b72c8f0ea54c22
lydat143 <lydat143@gmail.com>
2018-06-04 09:56:45 +0700

    Test

--- lib/db.php
+++ lib/db.php
@@ -1,8 +1,8 @@
 <?php
-	$host =
-	$user =
-	$pass =
-	$db =
+	$host = '127.0.0.1';
+	$user = 'web-HOANGLAMMOC';
+	$pass = 'web-HOANGLAMMOC';
+	$db = 'web-HOANGLAMMOC';
 	$link = mysqli_connect($host,$user,$pass,$db) or die ('Lỗi kết nối');
 	mysqli_query($link,'set names utf8');
 ?>
